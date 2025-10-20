d1457b1c49b68af8ff0c4a9743c2725b63cf5487
Luciano Alonzo <alonzoluciano85@gmail.com>
2025-10-20 17:40:30 -0300

    Arreglo en el nombre

--- src/components/itemlistcontainer.jsx
+++ src/components/itemlistcontainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ItemListContainer({ mensaje }) {
   return (
     <main className="container mt-4">
-      <h2>Bienvenido a MiTienda</h2>
+      <h2>Bienvenido a UruCel</h2>
       <p>{mensaje}</p>
     </main>
   );
